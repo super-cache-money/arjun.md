3f75551a7102c13b21e5e5146012359e3f71264b
Arjun Khoosal <arjunkhoosal@gmail.com>
2025-10-26 16:05:09 +0200

    two column looking good

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
     <html lang="en" className={`${sourceSerif.className}`}>
       <body className="antialiased tracking-tight">
         <div className="min-h-screen flex flex-col justify-between pt-0 md:pt-8 p-8 dark:bg-zinc-950 bg-white text-gray-900 dark:text-zinc-200">
-          <main className="max-w-[60ch] mx-auto w-full space-y-6">
+          <main className="max-w-[75ch] mx-auto w-full space-y-6">
             {children}
           </main>
           <Footer />
